c2987832f58177d8f833881665d5eb47170ee8b7
francischi <francischiooo@gmail.com>
2023-04-26 23:26:36 +0800

    2023/4/26 first commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# fastapi-websocket
-websocket with redis pubsub
+# redis + fastapi + websocket :zap::zap:
+
+**T**his is an example to show how to use websocket throught redis.
+The original way to use websocket in fastapi official document is **nice** to use when the project is small,
+but when it comes to a bigger project, you will add some cache policy into project such as 'redis', 
+or some load balance policy such as 'uvicorn workers','nginx load balancer',then you will have to make some effort on how to combine your application with those policies.
+
+**T**his project provide a more **efficient、scalable** way to let you use websocket combine with redis.
+
+**T**here are some benefits using redis as a pubsub service
+* extensibility
+* fast
+* wont be limit by programming language
+* normal ( its popular to use redis as a cache service )
+
+### How to use it
+* cd in this project
+  
+**run redis service** ```(if you have redis already u can skip this step)```
+* ~~~
+  ./redis/redis-server.exe
+**use pipenv run  main.py**
+* ~~~
+  pipenv shell
+* ~~~
+  pipenv install
+* ~~~
+  pipenv run uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+  ~~~
+
+### *notice
+make sure your redis serve on port 6379
+make sure your frontend code is something like below ("ws://127.0.0.1:8000/ws/user1")
+~~~
+<script>
+    var ws = new WebSocket("ws://127.0.0.1:8000/ws/user1");
+
+    ws.onmessage = function(event) {
+        var messages = document.getElementById('messages')
+        var message = document.createElement('li')
+        var content = document.createTextNode(event.data)
+        message.appendChild(content)
+        messages.appendChild(message)
+    };
+    function sendMessage(event) {
+        var input = document.getElementById("messageText")
+        ws.send(input.value)
+        input.value = ''
+        event.preventDefault()
+    }
+</script>
---
+++ main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI , WebSocket, WebSocketDisconnect
+from typing import List
+import time
+import threading
+import asyncio
+import uvicorn
+import aioredis
+
+app = FastAPI()
+@app.get('/')
+def hello():
+    return {
+        'message': 'fastapi is working'
+    }
+ 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        self.redis = aioredis.from_url("redis://127.0.0.1",encoding="utf-8",decode_responses = True)
+        self.existedConnection = False
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        if self.existedConnection == False:
+            self.existedConnection = True
+            await self.runRedisJob()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        if len(self.active_connections)==0:
+            self.existedConnection = False
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+    async def message2redis(self , message:str):
+        await self.redis.publish("chat:c", message)
+
+    async def runRedisJob(self):
+        async def listenRedis():
+            pubsub = self.redis.pubsub()
+            await pubsub.psubscribe("chat:c")
+            while self.existedConnection:
+                message = await pubsub.get_message()
+                if message:
+                    await self.broadcast(str(message['data']))
+                time.sleep(0.5)
+
+        t = threading.Thread(target=asyncio.run, args=(listenRedis(),))
+        t.start()
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.message2redis(data)
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+async def get_redis_pool():
+    redis = await aioredis.from_url("redis://127.0.0.1",encoding="utf-8",decode_responses = True)
+    try:
+        result = await redis.ping()
+    except:
+        raise SystemError('redis is not serving!')
+
+# -- confirm redis is working --
+@app.on_event("startup")
+async def startup_event():
+    await get_redis_pool()
+
+if __name__ == '__main__':
+    uvicorn.run(app , host = "127.0.0.1" , port = 8000 , workers=4)
+
+# if you dont have pipenv 
+# pip install pipenv
+# pipenv --three (create python3 virtual env)
+# pipenv install (install package in pipfile)
+# pipenv run uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
